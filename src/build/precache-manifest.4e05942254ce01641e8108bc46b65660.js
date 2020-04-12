d8f57b0968364003ebdc105f06d457e39a32770b
anuveyatsu <anuar.ustayev@gmail.com>
2020-04-13 00:37:48 +0600

    Update builds and release a new version of the dashboard.

--- src/build/precache-manifest.4e05942254ce01641e8108bc46b65660.js
+++ src/build/precache-manifest.4e05942254ce01641e8108bc46b65660.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "172ecbf2fb58d1ff081f353c46f77c7d",
+    "revision": "9232c33d0b4a15062f225ae0262ae0d2",
     "url": "/index.html"
   },
   {
-    "revision": "7018f62730159f3f2d3e",
+    "revision": "ac23956fbc5e8b70d05a",
     "url": "/static/css/main.d8fc8ea7.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.bf88c2cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7018f62730159f3f2d3e",
-    "url": "/static/js/main.a2151963.chunk.js"
+    "revision": "ac23956fbc5e8b70d05a",
+    "url": "/static/js/main.9677a42a.chunk.js"
   },
   {
     "revision": "374328e2ad3086bffad7",
